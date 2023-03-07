1814abc0ad3519b0a9e73c42ae80edc068d4110b
Antoine Jaussoin <antoine@jaussoin.com>
2023-03-07 22:10:12 +0000

    Another fix

--- frontend/docker/frontend-entrypoint.sh
+++ frontend/docker/frontend-entrypoint.sh
@@ -10,8 +10,7 @@ PREFIX='FRONTEND_'
 
 echo "window.__env__ = {" > "${CONFIG_FILE}"
 jq -n 'env' | grep "\"$PREFIX" >> "${CONFIG_FILE}"
-truncate -s -2 "${CONFIG_FILE}"
-echo "\n};" >> "${CONFIG_FILE}"
+echo "};" >> "${CONFIG_FILE}"
 sed -i "s#\"${PREFIX}#\"#g" "${CONFIG_FILE}"
 
 exec "$@"
