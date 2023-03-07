2ac684e543dbd243f7a0cf42b84b8f6483d02519
Antoine Jaussoin <antoine@jaussoin.com>
2023-03-07 22:03:28 +0000

    Fix

--- frontend/docker/frontend-entrypoint.sh
+++ frontend/docker/frontend-entrypoint.sh
@@ -8,24 +8,10 @@ envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.temp
 CONFIG_FILE='/usr/share/nginx/html/config.js'
 PREFIX='FRONTEND_'
 
-makeSedCommands() {
-  printenv | \
-      grep  "^$PREFIX" | \
-      sed -r "s/=/ /g" | \
-      xargs -n 2 sh -c 'echo "    \"$0\": \"$1\","'
-}
-
-# Set the delimiter to newlines (needed for looping over the function output)
-IFS=$'\n'
-
 echo "window.__env__ = {" > "${CONFIG_FILE}"
-for c in $(makeSedCommands); do
-  echo $c >> "${CONFIG_FILE}"
-done
+jq -n 'env' | grep "\"$PREFIX" >> "${CONFIG_FILE}"
 truncate -s -2 "${CONFIG_FILE}"
 echo "\n};" >> "${CONFIG_FILE}"
-
 sed -i "s#\"${PREFIX}#\"#g" "${CONFIG_FILE}"
 
-# Run any arguments passed to this script
 exec "$@"
