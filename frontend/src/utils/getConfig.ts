2d36074785e020d8385011d7db1322ca19845eb3
Antoine Jaussoin <antoine@jaussoin.com>
2023-03-08 18:23:54 +0000

    warn instad of error

--- frontend/src/utils/getConfig.ts
+++ frontend/src/utils/getConfig.ts
@@ -49,7 +49,7 @@ function getConfig(): Config {
 
   // Special cases
   if (!config.DEFAULT_LANGUAGE || config.DEFAULT_LANGUAGE.length !== 5) {
-    console.error(
+    console.warn(
       'Your default language (DEFAULT_LANGUAGE) is not in the right format. The right format should be a string of 5 characters, for example: en-GB, fr-FR, etc.'
     );
     config.DEFAULT_LANGUAGE = 'en-GB';
