fdaddb89603959e5322242958f83e612cedaa4fb
Antoine Jaussoin <antoine@jaussoin.com>
2023-03-08 19:46:52 +0000

    log

--- frontend/docker/frontend-entrypoint.sh
+++ frontend/docker/frontend-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
+echo "Starting frontend..."
+
 # Configure Nginx with backend host and port
 BACKEND_HOST="${BACKEND_HOST:-backend}" \
 BACKEND_PORT="${BACKEND_PORT:-3201}" \
@@ -11,18 +13,28 @@ CONFIG_FILE='/usr/share/nginx/html/config.tmp'
 HTML_FILE='/usr/share/nginx/html/index.html'
 PREFIX='FRONTEND_'
 
+echo "Configuring frontend with environment variables..."
+
 # Creates a file with the environment variables that start with FRONTEND_
 echo "    window.__env__ = {" > "${CONFIG_FILE}"
 jq -n 'env' | grep "\"$PREFIX" >> "${CONFIG_FILE}"
 echo "    };" >> "${CONFIG_FILE}"
 
+echo "> Finished configuring frontend with environment variables."
+
 # Removes the prefix from the environment variables
 sed -i "s#\"${PREFIX}#    \"#g" "${CONFIG_FILE}"
+
+echo "> Finished removing prefix from environment variables."
 
 # Injects the configuration file into the HTML file
 sed -i -e "/RUN-TIME CONFIGURATION/r ${CONFIG_FILE}" ${HTML_FILE}
 
+echo "> Finished injecting configuration file into HTML file."
+
 # Removes the temporary configuration file
 rm $CONFIG_FILE
+
+echo "> Finished removing temporary configuration file."
 
 exec "$@"
