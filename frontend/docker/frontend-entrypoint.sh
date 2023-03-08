3af2524c05b9a083ece93f15b0b703cd9b854e6e
Antoine Jaussoin <antoine@jaussoin.com>
2023-03-08 18:47:08 +0000

    another test

--- frontend/docker/frontend-entrypoint.sh
+++ frontend/docker/frontend-entrypoint.sh
@@ -6,11 +6,14 @@ BACKEND_PORT="${BACKEND_PORT:-3201}" \
 envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 CONFIG_FILE='/usr/share/nginx/html/config.js'
+HTML_FILE='/usr/share/nginx/html/index.html'
 PREFIX='FRONTEND_'
 
 echo "window.__env__ = {" > "${CONFIG_FILE}"
 jq -n 'env' | grep "\"$PREFIX" >> "${CONFIG_FILE}"
 echo "};" >> "${CONFIG_FILE}"
 sed -i "s#\"${PREFIX}#\"#g" "${CONFIG_FILE}"
+
+sed -i -e "/CONFIG_PLACEHOLDER/r ${CONFIG_FILE}" ${HTML_FILE}
 
 exec "$@"
